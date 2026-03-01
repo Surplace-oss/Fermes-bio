4a68dff5749567c7bfb30a29687fbd623cba116a
Surplace-oss <benjamin.teil64@gmail.com>
2026-03-01 11:12:30 +0100

    Update bio.js

--- api/bio.js
+++ api/bio.js
@@ -1,11 +1,20 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   const { departement = '91', page = 1 } = req.query;
+
   try {
     const url = `https://back.agencebio.org/api/operateurs?departement=${departement}&page=${page}&limit=100&actif=true`;
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: { 'Accept': 'application/json' }
+    });
+
+    if (!response.ok) throw new Error(`Agence Bio API error: ${response.status}`);
+
     const data = await response.json();
     res.status(200).json(data);
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
